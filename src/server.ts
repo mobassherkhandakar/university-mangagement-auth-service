73e06bfbd625b6a5c9e26bc5e83c8299d642ac54
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-10-30 18:58:57 +0600

    [Done] project husky added done

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
   try {
-    await mongoose.connect(config.database_url as string);
-    console.log("🛢️ database connection is started");
+    await mongoose.connect(config.database_url as string)
+    console.log('🛢️ database connection is started')
     app.listen(config.port, () => {
-      console.log(`university-management app listening on port ${config.port}`);
-    });
+      console.log(`university-management app listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log("🛢️database connection error", error);
+    console.log('🛢️database connection error', error)
   }
 }
 
-main();
+main()
