21906edf148739e4970c6f02943e1385c8b8e1f8
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-11-01 23:58:23 +0600

    eslint update

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
+import logger from './shared/logger/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('🛢️ database connection is started')
+    logger.info('🛢️ database connection is started')
     app.listen(config.port, () => {
-      console.log(`university-management app listening on port ${config.port}`)
+      logger.info(`university-management app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('🛢️database connection error', error)
+    logger.error('🛢️database connection error', error)
   }
 }
 
