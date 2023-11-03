f9cbc616f255dd13dbe521fb08fe71c19e9251fa
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-11-03 23:42:09 +0600

    global error handler error desing

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,13 +1,29 @@
+/* eslint-disable prefer-const */
 /* eslint-disable no-unused-vars */
 import { NextFunction, Request, Response } from 'express'
+import config from '../../config'
+import { IGenericErrorMessage } from '../interface/error'
 
 const globalErrorHandler = (
-  error,
+  error: Error,
   req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  res.status(400).json({ error: error })
+  let statusCode = 500
+  let message = 'something went wrong !'
+  let errorMessage: IGenericErrorMessage[] = []
+
+  if (error.name == 'ValidationError') {
+    // const simplifiedError = handleValidationError(error)
+  }
+
+  res.status(statusCode).json({
+    success: true,
+    message,
+    errorMessage,
+    stack: config.env !== 'production' ? error.stack : undefined,
+  })
   next()
 }
 
--- src/config/index.ts
+++ src/config/index.ts
@@ -7,4 +7,5 @@ export default {
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
   default_user_password: process.env.DEFAULT_USER_PASSWORD,
+  env: process.env.NODE_ENV,
 }
