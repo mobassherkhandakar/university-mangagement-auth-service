04fe629535338eabc067ddfd15f0f2280aa4b5ef
mobassherkhandakar <118150012+mobassherkhandakar@users.noreply.github.com>
2023-11-23 23:46:07 +0600

    dist folder remove

--- dist/app/modules/academicDepartment/academicDepartment.constants.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.academicDepartmentSearchAbleFields =
-  exports.academicDepartmentFilterAbleFields = void 0
-exports.academicDepartmentFilterAbleFields = ['searchTerm', 'title']
-exports.academicDepartmentSearchAbleFields = ['title']
--- dist/constants/pagination.js
+++
@@ -1,4 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.paginationSortFiled = void 0
-exports.paginationSortFiled = ['page', 'limit', 'sortBy', 'sortOrder']
